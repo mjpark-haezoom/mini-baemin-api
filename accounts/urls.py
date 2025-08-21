0bbe781d1039f707a64508609721ddd706880edb
Your Name <you@your-domain.com>
2025-08-21 16:53:13 +0900

    feat: Add signup API and Swagger docs
    This commit adds a new user signup API and configures
    Swagger documentation. It includes the user model serializer,
    view, and URL routing for the new endpoint.

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+# accounts/urls.py
+
+from django.urls import path
+
+from .views import UserRegisterView
+
+urlpatterns = [
+    path("signup/", UserRegisterView.as_view(), name="user-signup")
+]
--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,19 @@
+# backend/urls.py
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+# drf-spectacular 관련 import
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/v1/auth/", include("accounts.urls")),
+
+    # Swagger/Open API 관련 URL
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path(
+        'api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger-ui'
+    ),
 ]
