ac91e2a0c63681af12c23cd69a98e6640de3a1c3
Your Name <you@your-domain.com>
2025-08-22 16:00:45 +0900

    feat: added Login URLs

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,9 @@
 
 from django.urls import path
 
-from .views import UserRegisterView
+from .views import LoginView, UserRegisterView
 
 urlpatterns = [
-    path("signup/", UserRegisterView.as_view(), name="user-signup")
+    path("signup/", UserRegisterView.as_view(), name="user-signup"),
+    path("login/", LoginView.as_view(), name="user-login")
 ]
